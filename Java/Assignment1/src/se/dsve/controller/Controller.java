03e7e772294014d44590474bd85d2ccaec1341ee
Lars Strömberg <stromberg.lars@gmail.com>
2020-09-05 15:32:14 +0200

    Refactoring in Controller.java

--- Java/Assignment1/src/se/dsve/controller/Controller.java
+++ Java/Assignment1/src/se/dsve/controller/Controller.java
@@ -8,16 +8,6 @@ package se.dsve.controller;
 /\ \L\ \/\__, `\ \ \_/ |/\  __/
 \ \___,_\/\____/\ \___/ \ \____\
  \/__,_ /\/___/  \/__/   \/____/
-
-System.out.println("  __");
-System.out.println(" /\ \");
-System.out.println(" \_\ \    ____  __  __     __");
-System.out.println(" /'_` \  /',__\/\ \/\ \  /'__`\");
-System.out.println("/\ \L\ \/\__, `\ \ \_/ |/\  __/");
-System.out.println("\ \___,_\/\____/\ \___/ \ \____\");
-System.out.println(" \/__,_ /\/___/  \/__/   \/____/");
-System.out.println();
-
 */
 
 import se.dsve.model.Calculator;
