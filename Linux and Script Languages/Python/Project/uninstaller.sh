29da011fd83b4946fb9e801bc8bc315767bb1f56
Lars Strömberg <stromberg.lars@gmail.com>
2020-12-28 13:03:11 +0100

    Refactor order

--- Linux and Script Languages/Python/Project/uninstaller.sh
+++ Linux and Script Languages/Python/Project/uninstaller.sh
@@ -6,14 +6,14 @@ sudo systemctl status name_day.service
 # Stop Service
 sudo systemctl stop name_day.service
 
+# Delete Service
+sudo rm -d /lib/systemd/system/name_day.service
+
 # Reload systemd
 sudo systemctl daemon-reload
 
 # Status Service
 sudo systemctl status name_day.service
-
-# Delete Service
-sudo rm -d /lib/systemd/system/name_day.service
 
 # Delete directory in user home folder
 rm -r ~/name_day
