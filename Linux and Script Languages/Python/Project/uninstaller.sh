af53948d47926a23fb282d6d788a8554cf6d62fc
Lars Strömberg <stromberg.lars@gmail.com>
2020-12-28 12:26:30 +0100

    Refactor reload code

--- Linux and Script Languages/Python/Project/uninstaller.sh
+++ Linux and Script Languages/Python/Project/uninstaller.sh
@@ -6,11 +6,11 @@ sudo systemctl status name_day.service
 # Stop Service
 sudo systemctl stop name_day.service
 
-# Status Service
-sudo systemctl status name_day.service
-
 # Reload systemd
 sudo systemctl daemon-reload
+
+# Status Service
+sudo systemctl status name_day.service
 
 # Delete Service
 sudo rm -d /lib/systemd/system/name_day.service
