9f08285f914457e260ea04ebc895d1a96341ed4e
Lars Strömberg <stromberg.lars@gmail.com>
2020-12-28 12:12:46 +0100

    Curl script changes

--- Linux and Script Languages/Python/Project/__main__.py
+++ Linux and Script Languages/Python/Project/__main__.py
@@ -10,8 +10,8 @@ home = expanduser("~")
 def run_program():
     string = formatted_string(sort_out_relevant_data_from(response()))
 
-    # with open(f'{home}/name_day/name_days.txt', 'a') as name_file:  # Linux
-    with open('name_days.txt', 'a') as name_file:  # MacOS
+    with open(f'{home}/name_day/name_days.txt', 'a') as name_file:  # Linux
+    # with open('name_days.txt', 'a') as name_file:  # MacOS
         current_time = str(datetime.datetime.now()) + '\n'
         name_file.write(current_time)
         name_file.write(string)
