1c0f47b2c32dedec6df7a33ae0c755464aa4beea
Lars Strömberg <stromberg.lars@gmail.com>
2021-01-23 19:15:30 +0100

    Small use of MongoDB

--- Extras/Python REST API/web/app.py
+++ Extras/Python REST API/web/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Api, Resource
 
 from calculator import add, subtract, multiply, division
 
+from pymongo import MongoClient
+
 app = Flask(__name__)
 api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.aNewDB
+UserNum = db["UserNum"]
+
+UserNum.insert({
+    'num_of_users': 0
+})
+
+
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num + 1
+        UserNum.update({}, {"$set": {"num_of_users": new_num}})
+        return str("Hello user " + str(new_num))
 
 
 class Add(Resource):
@@ -32,11 +50,13 @@ api.add_resource(Add, "/add")
 api.add_resource(Subtract, "/subtract")
 api.add_resource(Multiply, "/multiply")
 api.add_resource(Divide, "/division")
+api.add_resource(Visit, "/hello")
 
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def index():
+    # return "Hello World!"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
